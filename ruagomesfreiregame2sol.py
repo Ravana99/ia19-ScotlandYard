be7d4c3b19759b067a1da8bdaae5cad639540fc9
ravana99 <tomas.lopes@tecnico.ulisboa.pt>
2019-11-29 02:22:06 +0000

    tests and changes to q learning

--- ruagomesfreiregame2sol.py
+++ ruagomesfreiregame2sol.py
@@ -1,5 +1,4 @@
 import random
-import numpy
 
 # LearningAgent to implement
 # no knowledeg about the environment can be used
@@ -11,7 +10,6 @@ class LearningAgent:
 	# nA maximum number of action per state
 	def __init__(self,nS,nA):
 
-		# define this function
 		self.nS = nS
 		self.nA = nA
 
@@ -19,19 +17,22 @@ class LearningAgent:
 		self.nvisits = [[0]*nA for i in range(nS)]
 		self.gamma = 1
 		self.nactions = [-1]*nS
+		self.epsilon = 0.5
+
 		  
 	
 	# Select one action, used when learning  
-	# st - is the current state        
+	# st - is the current state  
 	# aa - is the set of possible actions
 	# for a given state they are always given in the same order
 	# returns the index to the action in aa
 	def selectactiontolearn(self,st,aa):
-		# define this function
-		# print("select one action to learn better")
 		if self.nactions[st] == -1:
 			self.nactions[st] = len(aa)
-		return random.randint(0, self.nactions[st]-1)
+		if random.random()>=self.epsilon:
+			return self.selectactiontoexecute(st,aa)
+		else:
+			return random.randint(0, self.nactions[st]-1)
 
 	# Select one action, used when evaluating
 	# st - is the current state        
@@ -40,7 +41,6 @@ class LearningAgent:
 	# returns
 	# imax - the index to the action in aa
 	def selectactiontoexecute(self,st,aa):
-		# define this function
 		if self.nactions[st] == -1:
 			self.nactions[st] = len(aa)
 		amax = self.qtable[st][0]
@@ -50,8 +50,6 @@ class LearningAgent:
 				amax = self.qtable[st][i]
 				imax = i
 
-		# print("select one action to see if I learned")
-		print(st)
 		return imax
 
 
@@ -61,19 +59,23 @@ class LearningAgent:
 	# a - the index to the action taken
 	# r - reward obtained
 	def learn(self,ost,nst,a,r):
-		# define this function
-		#print("learn something from this data")
-		#if self.nvisits[ost][a] == 0:
-			#self.qtable[ost][a] = r
+
+		#KEEP??
+		if self.nvisits[ost][a] == 0:
+			self.qtable[ost][a] = r
+
 		self.nvisits[ost][a] += 1
+
+
 		alphan = 1/(self.nvisits[ost][a]+1)
+
 		amax = self.qtable[nst][0]
 		if self.nactions[nst] == -1:
 			amax = 0
-		else:	
-			for i in range(1, self.nactions[nst]):
-				if amax < self.qtable[nst][i]:
-					amax = self.qtable[nst][i]
+
+		for i in range(1, self.nactions[nst]):
+			if amax < self.qtable[nst][i]:
+				amax = self.qtable[nst][i]
 
 		self.qtable[ost][a] = (1-alphan)*self.qtable[ost][a] + alphan*(r+self.gamma*amax)
 
---
+++ test2.py
@@ -0,0 +1,151 @@
+import pickle
+import random
+import matplotlib.pyplot as plt
+from ruagomesfreiregame2sol import *
+
+def runagent(A, T, R, I = 1, learningphase=True, nlearn = 1000, ntest = 100):
+
+        J = 0 #J -> sum of all rewards received
+        if learningphase:
+                n = nlearn #n -> number of tests
+        else:
+                n = ntest #n -> number of learns
+                
+        st = I #state = I (I -> initial state)
+        for ii in range(1,n): #executes n tests/learns
+                aa = T[st][0] #possible next states after the current state st
+                if learningphase:
+                        a = A.selectactiontolearn(st,aa) #a -> action to learn
+                else:
+                        a = A.selectactiontoexecute(st,aa) #a -> action to execute
+                try:
+                        nst = T[st][0][a] # nst -> next state using the selected action a
+                except:
+                        print(st,a)
+                r = R[st] #r -> reward associated with the current state st
+                J += r #adds current reward to the sum of all rewards received
+                #print(st,nst,a,r)
+
+                if learningphase:
+                        A.learn(st,nst,a,r)
+                else:
+                        #print(st,nst,a,r)
+                        pass
+                
+                st = nst #state = next state
+
+                if not ii%15: #after 15 iterations return to the intial state
+                        st = I
+        return J/n #returns average of all rewards received by number of tests/learns
+        
+
+# due to the randomness in the learning process, we will run everything NREP times
+# the final grades are based on the average on all of them
+
+NIT = 5
+NREP = 100 #number of times the whole process will be executed
+hits0 = 0
+hits1 = 0
+hits2 = 0
+hits3 = 0
+hitsu0 = 0
+hitsu1 = 0
+hitsu2 = 0
+hitsu3 = 0
+
+with open("mapasgraph2.pickle", "rb") as fp:
+        AA = pickle.load(fp)
+
+for i in range(NREP):
+
+        val = [0,0,0,0]
+
+        for nrep in range(NIT):
+
+                A = LearningAgent(114,15)
+
+                T = AA[0]
+                R = [-1]*114
+                R[7] = 1
+                R[1] = 0
+                R[2] = 0
+                R[3] = 0
+                R[4] = 0
+
+                runagent(A, T, R, I = 1, learningphase=True, nlearn = 500)
+                Jn = runagent(A, T, R, I = 1, learningphase=False, ntest = 10)
+                val[0] += Jn
+
+                if Jn >= 0.3:
+                    hitsu0 += 1
+
+
+                runagent(A, T, R, I = 1, learningphase=True, nlearn = 10000)
+                Jn = runagent(A, T, R, I = 1, learningphase=False, ntest = 10)
+                val[1] += Jn
+
+                if Jn >= 0.3:
+                    hitsu1 += 1
+
+        for nrep in range(NIT):
+                
+                A = LearningAgent(114,15)
+
+                T = AA[0]
+                R = [-1]*114
+                R[10] = 1
+
+                runagent(A, T, R, I = 1, learningphase=True, nlearn = 1000)
+                Jn = runagent(A, T, R, I = 1, learningphase=False, ntest = 10)
+                val[2] += Jn
+
+                if Jn >= -0.85:
+                    hitsu2 += 1
+                
+                runagent(A, T, R, I = 1, learningphase=True, nlearn = 10000)
+                Jn = runagent(A, T, R, I = 1, learningphase=False, ntest = 10)
+                val[3] += Jn
+
+                if Jn >= -0.6:
+                    hitsu3 += 1
+
+
+        val = [val[i]/NIT for i in range(4)]
+
+        if val[0]>=0.3:
+            hits0 += 1
+        #else:
+            #print("MISS CONJUNTO NO EXEMPLO 1 TESTE 1")
+        if val[1]>=0.3:
+            hits1 += 1
+        #else:
+            #print("MISS CONJUNTO NO EXEMPLO 1 TESTE 2")
+        if val[2]>=-0.85:
+            hits2 += 1
+        #else:
+            #print("MISS CONJUNTO NO EXEMPLO 2 TESTE 1")
+        if val[3]>=-0.6:
+            hits3 += 1
+        #else:
+            #print("MISS CONJUNTO NO EXEMPLO 2 TESTE 2")
+
+
+print("RESULTADOS ÚNICOS:")
+print("Hits exemplo 1 teste 1:" +  str(hitsu0) + " em " + str(NREP*NIT))
+print("% Hits exemplo 1 teste 1: " + str(hitsu0*100/(NREP*NIT)) + "%")
+print("Hits exemplo 1 teste 2:" +  str(hitsu1) + " em " + str(NREP*NIT))
+print("% Hits exemplo 1 teste 2: " + str(hitsu1*100/(NREP*NIT)) + "%")
+print("Hits exemplo 2 teste 1:" +  str(hitsu2) + " em " + str(NREP*NIT))
+print("% Hits exemplo 2 teste 1: " + str(hitsu2*100/(NREP*NIT)) + "%")
+print("Hits exemplo 2 teste 2:" +  str(hitsu3) + " em " + str(NREP*NIT))
+print("% Hits exemplo 2 teste 2: " + str(hitsu3*100/(NREP*NIT)) + "%")
+
+print("RESULTADOS CONJUNTOS:")
+print("Hits exemplo 1 teste 1:" +  str(hits0) + " em " + str(NREP))
+print("% Hits exemplo 1 teste 1: " + str(hits0*100/NREP) + "%")
+print("Hits exemplo 1 teste 2:" +  str(hits1) + " em " + str(NREP))
+print("% Hits exemplo 1 teste 2: " + str(hits1*100/NREP) + "%")
+print("Hits exemplo 2 teste 1:" +  str(hits2) + " em " + str(NREP))
+print("% Hits exemplo 2 teste 1: " + str(hits2*100/NREP))
+print("Hits exemplo 2 teste 2:" +  str(hits3) + " em " + str(NREP))
+print("% Hits exemplo 2 teste 2: " + str(hits3*100/NREP))
